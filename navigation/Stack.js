6042347e3b381ffac4a1c016d61bc21d2d64d2c1
ARMkazanjian <garikkazan@aol.com>
2025-11-13 17:03:40 -0500

    remove react-native-maps and simplify navigation for web compatibility

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,68 +1,35 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Map from "../screens/Map";
-import Home from "../screens/Home";
-import SubmissionDetails from "../screens/SubmissionDetails";
-import SightingDetails from "../screens/SightingDetails";
+import Upload from "../screens/upload";
 import DetailsScreen from "../screens/details";
-import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
-import { Pressable} from "react-native-gesture-handler";
-import { StyleSheet } from "react-native";
 
 const Stack = createStackNavigator();
 
 export default function AppStack() {
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{
-    headerStyle: {
-      backgroundColor: "#dcd8ce",
-    }, headerTintColor: "#4c5345", 
-    headerTitleStyle: {
-      fontWeight: "bold",
-    },}}>
+    <Stack.Navigator initialRouteName="Upload" screenOptions={{
+      headerStyle: {
+        backgroundColor: "#dcd8ce",
+      }, 
+      headerTintColor: "#4c5345", 
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+    }}>
       <Stack.Screen 
-        name="Home" 
-        component={Home} 
+        name="Upload" 
+        component={Upload} 
         options={{
-          title:"Home",
-          headerRight: () => (
-            <Pressable style={styles.profileButton} onPress={() => console.log("Profile pressed")}>
-              <Ionicons name="person-outline" size={20} color="#EAE2DC" />
-            </Pressable>
-          ),
-        }} />
+          title: "Solidago Species Identifier",
+        }} 
+      />
       <Stack.Screen 
-        name = "Details"
+        name="Details"
         component={DetailsScreen}
         options={{
           title: "Plant Details",
-        }} />
-      <Stack.Screen 
-        name="SubmissionDetails" 
-        component={SubmissionDetails} 
-        options={{
-          title: "Confirm Sighting", 
-          tabBarStyle: { display: "none" } 
-        }}/>
-      <Stack.Screen name="Map" component={Map} />
-      <Stack.Screen 
-        name="SightingDetails" 
-        component={SightingDetails} 
-        options={{
-          tabBarStyle: { display: "none" } 
-        }}/>
+        }} 
+      />
     </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  profileButton: {
-    backgroundColor: "#4c5345",
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    marginRight: 15,
-  },
-});
